d9984e670a6cc1764569aea5fbd64829a5ce7bcb
siddharthasharma79 <siddharthasharma79@gmail.com>
2018-01-09 14:13:25 +0530

    adding array practice page

---
+++ src/main/java/com/sidd/javapractice/ArrayPractice.java
@@ -0,0 +1,22 @@
+package com.sidd.javapractice;
+
+import java.util.Scanner;
+
+public class ArrayPractice {
+
+	public static void main(String[] args) {
+		
+		int[] arr = new int[5];
+		arr[0] = 1;
+		
+		Scanner	sc = new Scanner(System.in);
+		int in = sc.nextInt();
+
+	}
+
+}
+
+class a {
+	
+}
+		
